e68fe23900689a954783ab1ee1e0d94dc9a745c8
Aws Ayyash <74304034+AwsAyyash@users.noreply.github.com>
2025-12-29 18:58:52 +0200

    a

--- hello.py
+++ hello.py
@@ -8,5 +8,6 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))  # ✅ Reads PORT
+    app.run(host='0.0.0.0', port=port)
 
